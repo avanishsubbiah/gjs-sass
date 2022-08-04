bb6feebc225b82afba8966e1556dc420511772a6
avanishsubbiah <avanishsubbiah1@gmail.com>
2022-08-04 18:56:03 -0400

    removed unecessary self.Object

--- sass_test/sussyfs.js
+++ sass_test/sussyfs.js
@@ -2,25 +2,29 @@ import * as fs from 'fs';
 
 export class sussyfs {
     static readFileSync(path, encoding) {
+        console.log(encoding);
         return fs.readFileSync(path, encoding);
     }
     static writeFileSync(path, contents) {
-        return fs.writeFileSync(path, contents);
+        fs.writeFileSync(path, contents);
     }
     static existsSync(path) {
         return true;
         // return fs.existsSync(path);
     }
     static unlinkSync(path) {
-        return fs.unlinkSync(path);
+        fs.unlinkSync(path);
     }
-    static readdirSync(path) {
-        return fs.readdirSync(path);
-    }
+    //static readdirSync(path) {
+    //	console.log("RAN READDIR--------------------------------");
+    //	console.log(fs.readdirSync(path));
+    //    return fs.readdirSync(path);
+    //}
     static statSync(path) {
+	console.log(fs.statSync(path));
         return fs.statSync(path);
     }
     static mkdirSync(path) {
-        return fs.mkdirSync(path);
+        fs.mkdirSync(path);
     }
 }
